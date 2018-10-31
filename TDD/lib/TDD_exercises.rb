6b030f9d7c5f72685435d29b8afe8e43fc70a123
le-s <steven.an.le@gmail.com>
2018-10-31 14:10:20 -0700

    Create specs for #two_sum, #my_transpose, and #stock_picker

--- TDD/lib/TDD_exercises.rb
+++ TDD/lib/TDD_exercises.rb
@@ -8,4 +8,30 @@ class Array
     result
   end
   
+  def two_sum
+    result = []
+    
+    i = 0
+    while i < self.length - 1
+      j = i + 1
+      while j < self.length
+        result << [i,j]  if self[i] + self[j] == 0
+        j += 1
+      end
+      i += 1
+    end
+
+    result
+  end
+  
+  def my_transpose
+    result = [[],[],[]]
+    self.each do |row|
+      (0..2).each do |col_idx|
+        result[col_idx] << row[col_idx]
+      end
+    end
+    
+    result
+  end
 end
--- TDD/spec/TDD_exercises_spec.rb
+++ TDD/spec/TDD_exercises_spec.rb
@@ -15,6 +15,34 @@ RSpec.describe Array do
       end
     end
   end
+  
+  describe "#two_sum" do
+    context "array contains pairs that sum to zero" do
+      it "return pairs when sum of pair is equal to zero" do
+        expect([-1,0,2,-2,1].two_sum).to eq([[0,4], [2,3]])
+      end
+    end
+    
+    context "array doesn't contain pairs that sum to zero" do
+      it "returns an empty array" do
+        expect([-6,2,-15,4,-5].two_sum).to be_empty
+      end 
+    end
+  end
+  
+  describe "my_transpose" do 
+    array = [[0,1,2],[3,4,5],[6,7,8]]
+    it "convert betweeen row-oriented & column-oriented" do
+      expect(array.my_transpose).to eq([[0,3,6],[1,4,7],[2,5,8]])
+    end
+  end
+  
+  describe "stock_picker" do
+    array = [10,9,8,11,5]
+    it "finds the most profitable pair" do
+      expect(array.stock_picker).to eq([2,3])
+    end
+  end 
 end
 
   
